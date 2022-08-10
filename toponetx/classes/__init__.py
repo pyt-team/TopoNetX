5dc1954a20811beef77eaa9633110de5651e67ea
Mustafa Hajij <61298952+mhajij@users.noreply.github.com>
2022-08-09 17:53:14 -0700

    Update __init__.py

--- toponetx/classes/__init__.py
+++ toponetx/classes/__init__.py
@@ -2,4 +2,5 @@ from .rankedentity import RankedEntity, RankedEntitySet
 from .simplicialcomplex import SimplicialComplex
 from .cellcomplex import Cell, CellComplex
 from .combinatorialcomplex import CombinatorialComplex
-from .cellcomplex import Cell, CellComplex
+from .cellcomplex import CellComplex
+from .cell import Cell, CellView
