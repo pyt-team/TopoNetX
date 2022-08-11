5d4f024d0c8626e8337535c1c3192ec5cd636fe1
Jan <philipp.meissner@rwth-aachen.de>
2022-08-11 21:17:01 +0200

    Added `decorator` library for `hypernetx`

--- setup.py
+++ setup.py
@@ -14,7 +14,15 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-install_requires = ["gudhi", "networkx", "hypernetx", "numpy", "pre-commit", "scipy"]
+install_requires = [
+    "gudhi",
+    "decorator",
+    "networkx",
+    "hypernetx",
+    "numpy",
+    "pre-commit",
+    "scipy",
+]
 
 full_requires = []
 
