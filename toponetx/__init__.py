23cc9155382e2057f25a5be18857f3f5691bfc5e
Mustafa Hajij <mustafa.hajij@gmail.com>
2023-01-12 09:28:24 -0800

    embedding features removed

--- toponetx/__init__.py
+++ toponetx/__init__.py
@@ -14,12 +14,6 @@ from .classes.abstract_cell import AbstractCellView, AbstractCell
 from .classes.cell import Cell, CellView
 from .classes.node_view import NodeView
 
-from .embedding.cell2vec import Cell2Vec
-from .embedding.deepcell import DeepCell
-from .embedding.cell_diff2vec import CellDiff2Vec
-from .embedding.higher_order_geometric_laplacian_eigenmaps import HOGLEE
-from .embedding.higher_order_laplacian_eigenmaps import HigherOrderLaplacianEigenmaps
-
 from .utils.structure import (
     sparse_array_to_neighborhood_list,
     neighborhood_list_to_neighborhood_dict,
--- toponetx/embedding/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  9 21:01:06 2022
-
-@author: musta
-"""
