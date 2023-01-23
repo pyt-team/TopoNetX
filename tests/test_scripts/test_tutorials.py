1ce6f8d82cf94da29bff852143dadc9974aa09a9
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-01-23 22:05:36 +0000

    First draft of test_tutorials.py

---
+++ tests/test_scripts/test_tutorials.py
@@ -0,0 +1,44 @@
+"""Unit tests for the tutorials."""
+
+import glob
+import json
+import os
+import subprocess
+import tempfile
+
+import pytest
+
+BACKEND = os.environ.get("TOPONETX_BACKEND", "numpy")
+ALL_BACKENDS = ["numpy"]
+
+def _exec_tutorial(path):
+
+    file_name = tempfile.NamedTemporaryFile(suffix=".ipynb").name
+    args = [
+        "jupyter",
+        "nbconvert",
+        "--to",
+        "notebook",
+        "--execute",
+        "--ExecutePreprocessor.timeout=1000",
+        "--ExecutePreprocessor.kernel_name=python3",
+        "--output",
+        file_name,
+        path,
+    ]
+    subprocess.check_call(args)
+
+TUTORIALS_DIR = "tutorials"
+paths = sorted(glob.glob(f"{TUTORIALS_DIR}/*.ipynb"))
+
+
+@pytest.mark.parametrize("path", paths)
+def test_tutorial(path):
+    with open(path, "r") as file:
+        metadata = json.load(file).get("metadata")
+
+    backends = metadata.get("backends", ALL_BACKENDS)
+    if BACKEND not in backends:
+        pytest.skip()
+
+    _exec_tutorial(path)
