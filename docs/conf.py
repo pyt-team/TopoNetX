e3284d4dcc2cc93528c89a3c8d205b05b2eab92b
Nina Miolane <nmiolane@gmail.com>
2023-05-04 17:54:01 -0700

    Add nbsphinx_prolog

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,14 @@ master_doc = "index"
 
 language = None
 
+nbsphinx_prolog = r"""
+{% set docname = env.doc2path(env.docname, base=None) %}
+
+.. raw:: latex
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+    """
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 pygments_style = None
