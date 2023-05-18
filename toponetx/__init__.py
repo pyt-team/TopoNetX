1c8f7a170c412f2abc9030d97c620978be5ae8a9
Mustafa Hajij <mustafa.hajij@gmail.com>
2023-05-17 23:46:36 -0700

    delete dynamic cc

--- toponetx/__init__.py
+++ toponetx/__init__.py
@@ -9,11 +9,8 @@ from toponetx.exception import (
 from .classes.complex import Complex
 from .classes.simplicial_complex import SimplicialComplex
 from .classes.cell_complex import CellComplex
-from .classes.dynamic_combinatorial_complex import DynamicCombinatorialComplex
 from .classes.combinatorial_complex import CombinatorialComplex
 
-from .classes.ranked_entity import RankedEntity, RankedEntitySet
-from .classes.dynamic_cell import DynamicCell
 from .classes.simplex import Simplex
 from .classes.hyperedge import HyperEdge
 from .classes.cell import Cell
--- toponetx/classes/__init__.py
+++ toponetx/classes/__init__.py
@@ -1,5 +1,3 @@
-from .ranked_entity import *
-from .dynamic_cell import *
 from .cell import *
 from .simplex import *
 from .hyperedge import *
@@ -11,5 +9,4 @@ from .reportview import *
 from .complex import *
 from .simplicial_complex import *
 from .cell_complex import *
-from .dynamic_combinatorial_complex import *
 from .combinatorial_complex import *
