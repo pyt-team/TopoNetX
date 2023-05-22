fdd0ad656394ee6e3a029a6821035ea12ccc0397
Nina Miolane <nmiolane@gmail.com>
2023-05-22 06:50:50 -1000

    Address D200s in cell.py

--- toponetx/classes/cell.py
+++ toponetx/classes/cell.py
@@ -15,47 +15,48 @@ __all__ = ["Cell"]
 class Cell:
     r"""Class representing a 2D cell.
 
-     A 2D cell is an elementary building block used to build a 2D cell complex, whether regular or non-regular.
+    A 2D cell is an elementary building block used to build a 2D cell complex, whether regular or non-regular.
 
-     Parameters
-     ----------
-     elements : iterable of hashable objects
-         An iterable that contains hashable objects representing the nodes of the cell. The order of the elements is important
-         and defines the cell up to cyclic permutation.
-     name : str, optional
-         A string representing the name of the cell. The default value is None.
-     regular : bool, optional
-         A boolean indicating whether the cell satisfies the regularity condition. The default value is True.
-         A 2D cell is regular if and only if there is no repetition in the boundary edges that define the cell.
-         By default, the cell is assumed to be regular unless otherwise specified. Self-loops are not allowed in the boundary
-         of the cell. If a cell violates the cell complex regularity condition, a ValueError is raised.
-     **attr : keyword arguments, optional
-         Properties belonging to the cell can be added as key-value pairs. Both the key and value must be hashable.
+    Parameters
+    ----------
+    elements : iterable of hashable objects
+        An iterable that contains hashable objects representing the nodes of the cell. The order of the elements is important
+        and defines the cell up to cyclic permutation.
+    name : str, optional
+        A string representing the name of the cell. The default value is None.
+    regular : bool, optional
+        A boolean indicating whether the cell satisfies the regularity condition. The default value is True.
+        A 2D cell is regular if and only if there is no repetition in the boundary edges that define the cell.
+        By default, the cell is assumed to be regular unless otherwise specified. Self-loops are not allowed in the boundary
+        of the cell. If a cell violates the cell complex regularity condition, a ValueError is raised.
+    **attr : keyword arguments, optional
+        Properties belonging to the cell can be added as key-value pairs. Both the key and value must be hashable.
 
-     Notes
-     -----
-     - A cell is defined as an ordered sequence of nodes (n1, ..., nk), where each two consecutive nodes (ni, ni+1)
-       define an edge in the boundary of the cell. Note that the last edge (nk, n1) is also included in the boundary
-       of the cell and is used to close the cell. For instance, if a Cell is defined as `c = Cell((1, 2, 3))`,
-       then `c.boundary` will return `[(1, 2), (2, 3), (3, 1)]`, which consists of three edges.
+    Notes
+    -----
+    - A cell is defined as an ordered sequence of nodes (n1, ..., nk), where each two consecutive nodes (ni, ni+1)
+    define an edge in the boundary of the cell. Note that the last edge (nk, n1) is also included in the boundary
+    of the cell and is used to close the cell. For instance, if a Cell is defined as `c = Cell((1, 2, 3))`,
+    then `c.boundary` will return `[(1, 2), (2, 3), (3, 1)]`, which consists of three edges.
     - When cell is created, its boundary is automatically created as a
-       set of edges that encircle the cell.
-     Examples
-     --------
-     >>> cell1 = Cell((1, 2, 3))
-     >>> cell2 = Cell((1, 2, 4, 5), weight=1)
-     >>> cell3 = Cell(("a", "b", "c"))
-     >>> # create geometric cell:
-     >>> v0 = (0, 0)
-     >>> v1 = (1, 0)
-     >>> v2 = (1, 1)
-     >>> v3 = (0, 1)
-     # create the cell with the vertices and edges
-     >>> cell = Cell([v0, v1, v2, v3],type="square")
-     >>> cell["type"]
-     >>> print(list(cell.boundary))
-     [((0, 0), (1, 0)), ((1, 0), (1, 1)), ((1, 1), (0, 1)),
-      ((0, 1), (0, 0))]
+    set of edges that encircle the cell.
+
+    Examples
+    --------
+    >>> cell1 = Cell((1, 2, 3))
+    >>> cell2 = Cell((1, 2, 4, 5), weight=1)
+    >>> cell3 = Cell(("a", "b", "c"))
+    >>> # create geometric cell:
+    >>> v0 = (0, 0)
+    >>> v1 = (1, 0)
+    >>> v2 = (1, 1)
+    >>> v3 = (0, 1)
+    # create the cell with the vertices and edges
+    >>> cell = Cell([v0, v1, v2, v3],type="square")
+    >>> cell["type"]
+    >>> print(list(cell.boundary))
+    [((0, 0), (1, 0)), ((1, 0), (1, 1)), ((1, 1), (0, 1)),
+    ((0, 1), (0, 0))]
     """
 
     def __init__(self, elements, name=None, regular=True, **attr):
@@ -134,10 +135,10 @@ class Cell:
 
     @property
     def is_regular(self):
-        r"""
-        Returns true is the Cell is a regular cell, and False otherwise
-        """
+        r"""Check if a cell is regular.
 
+        Return true is the Cell is a regular cell, and False otherwise
+        """
         if self._regular:  # condition enforced
             return True
         else:
@@ -172,20 +173,21 @@ class Cell:
     def sign(self, edge):
         r"""The sign method of the Cell class takes an edge as input and returns the sign of the edge with respect to the cell. If the edge is in the boundary of the cell, then the sign is 1 if the edge is in the counterclockwise direction around the cell and -1 if it is in the clockwise direction. If the edge is not in the boundary of the cell, a KeyError is raised.
 
-        Args:
-
+        Parameters
+        ----------
         edge: an iterable representing the edge whose sign with respect to the cell is to be computed.
-        Returns:
+
+        Returns
+        -------
 
         1: if the edge is in the boundary of the cell and is in the counterclockwise direction around the cell.
         -1: if the edge is in the boundary of the cell and is in the clockwise direction around the cell.
-        Raises:
 
+        Raises
+        ------
         KeyError: if the input edge is not in the boundary of the cell.
         ValueError: if the input edge is not iterable or if its length is not 2.
-
         """
-
         if isinstance(edge, Iterable):
             if len(edge) == 2:
                 if tuple(edge) in self.boundary:
@@ -205,8 +207,8 @@ class Cell:
         return e in self._elements
 
     def __repr__(self):
-        """
-        String representation of regular cell
+        """String representation of regular cell.
+
         Returns
         -------
         str
@@ -215,11 +217,13 @@ class Cell:
 
     @property
     def boundary(self):
-        r"""
-        a 2d cell is characterized by its boundary edges
-        Return
-        ------
-        iterator of tuple representing boundary edges given in cyclic order
+        r"""Boundary.
+
+        A 2d cell is characterized by its boundary edges.
+
+        Returns
+        -------
+        Iterator of tuple representing boundary edges given in cyclic order.
         """
         return iter(self._boundary)
 
@@ -241,40 +245,40 @@ class Cell:
         return c
 
     def is_homotopic_to(self, cell):
-        """
+        """Check if self is homotopic to input cell.
+
         Parameters
         ----------
         cell : tuple, list or Cell
 
-        Return
-        ------
+        Returns
+        -------
         _ : bool
             Return True is self is homotopic to input cell and False otherwise.
         """
-
         return Cell._are_homotopic(self, cell) or Cell._are_homotopic(
             self.reverse(), cell
         )
 
     @staticmethod
     def _are_homotopic(cell1, cell):
-        """
-        Parameters:
-        ---------
+        """Check if cell1 is homotopic to input cell.
+
+        Parameters
+        ----------
         cell1 : Cell
         cell : tuple, list or Cell
 
-        Return : bool
-        ------
-            return True is self is homotopic to input cell and False otherwise.
-
-        Note :
+        Returns
         -------
-            in a 2d-cell complex, two 2d-cells are homotopic iff one
-                of them can be obtaine from the other by a cylic rotation
-                of the boundary verties defining the 2d cells.
-        """
+        return True is self is homotopic to input cell and False otherwise.
 
+        Notes
+        -----
+        in a 2d-cell complex, two 2d-cells are homotopic iff one
+            of them can be obtaine from the other by a cylic rotation
+            of the boundary verties defining the 2d cells.
+        """
         if isinstance(cell, tuple) or isinstance(cell, list):
             seq = cell
         elif isinstance(cell, Cell):
@@ -302,8 +306,8 @@ class Cell:
         return False
 
     def __str__(self):
-        """
-        String representation of regular cell
+        """String representation of regular cell.
+
         Returns
         -------
         str
