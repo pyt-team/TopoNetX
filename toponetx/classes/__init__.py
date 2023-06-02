87a079798fa282eed72dc4da250d74bc586fa298
Josef Hoppe <hoppe@cs.rwth-aachen.de>
2023-06-02 10:42:49 +0200

    Remove isort ignore of __init__.py files

--- toponetx/classes/__init__.py
+++ toponetx/classes/__init__.py
@@ -1,11 +1,8 @@
 from .cell import *
-from .simplex import *
-from .hyperedge import *
-
-
-from .reportviews import *
-
-from .complex import *
-from .simplicial_complex import *
 from .cell_complex import *
 from .combinatorial_complex import *
+from .complex import *
+from .hyperedge import *
+from .reportviews import *
+from .simplex import *
+from .simplicial_complex import *
