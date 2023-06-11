127abbe8a6d6a39d8166ab45548b09824b519740
Guru G <gurushankar.gopalki@gmail.com>
2023-06-10 19:50:56 -0700

    test cases for hodge_laplacian_matrix, with different ranks

--- test/classes/test_simplicial_complex.py
+++ test/classes/test_simplicial_complex.py
@@ -388,6 +388,36 @@ class TestSimplicialComplex(unittest.TestCase):
 
         self.assertDictEqual(row, expected_row)
 
+        # compute Hodge Laplacian for different ranks
+        column, L_hodge = SC.hodge_laplacian_matrix(rank=2, signed=False, index=True)
+        expected_col = {(1, 2, 3): 0, (2, 3, 4): 1}
+        self.assertDictEqual(column, expected_col)
+        np.testing.assert_array_equal(L_hodge.A, np.array([[3, 1], [1, 3]]))
+
+        column, L_hodge = SC.hodge_laplacian_matrix(rank=1, signed=False, index=True)
+        expected_col = {
+            (0, 1): 0,
+            (1, 2): 1,
+            (1, 3): 2,
+            (2, 3): 3,
+            (2, 4): 4,
+            (3, 4): 5,
+        }
+        self.assertDictEqual(column, expected_col)
+        np.testing.assert_array_equal(
+            L_hodge.A,
+            np.array(
+                [
+                    [2.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                    [1.0, 3.0, 0.0, 0.0, 1.0, 0.0],
+                    [1.0, 0.0, 3.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 4.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0, 3.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0, 0.0, 3.0],
+                ]
+            ),
+        )
+
     def test_adjacency_matrix(self):
         """Test adjacency_matrix shape and values."""
         SC = SimplicialComplex([[1, 2, 3], [2, 3, 4], [0, 1]])
