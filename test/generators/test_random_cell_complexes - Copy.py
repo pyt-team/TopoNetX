34827e54936a108c454f9bdd400d145f01551bf0
your.user.name <Ialjab2@lsu.edu>
2023-11-02 09:46:25 -0700

    fixing testing

--- test/generators/test_random_cell_complexes - Copy.py
+++ test/generators/test_random_cell_complexes - Copy.py
@@ -8,16 +8,15 @@ class TestCcNpComplex:
     def test_zero_nodes(self):
         """Test `cc_np_complex` for zero nodes."""
         cc = cc_np_complex(0, 0.1)
-        assert cc.shape == (0,0,0)
+        assert cc.shape == (0, 0, 0)
 
     def test_non_zero_nodes(self):
         """Test `cc_np_complex` for nonzero nodes."""
         cc = cc_np_complex(5, 0.1)
         assert cc.shape[0] == 5
-        
-    def test_non_zero_nodes(self):
+
+    def test_zero_probability(self):
         """Test `cc_np_complex` for zero probability."""
         cc = cc_np_complex(40, 0)
         assert cc.shape[0] == 40
-
         assert cc.shape[-1] == 0
