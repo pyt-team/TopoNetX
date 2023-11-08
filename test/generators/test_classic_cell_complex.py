d623830a7eb97105c3be5a54872bc47d33ba2720
your.user.name <Ialjab2@lsu.edu>
2023-11-07 20:05:30 -0800

    adding classic cell complex

---
+++ test/generators/test_classic_cell_complex.py
@@ -0,0 +1,23 @@
+"""Test the classic cell complex generators."""
+from toponetx.generators.classic_cell_complexes import (
+    single_cell_complex,
+    pyrmaid_complex
+)
+
+
+class TestSingleCellComplex:
+    """Test the `single_cell_complex` function."""
+
+    def test_single_cell(self):
+        """Test `single_cell_complex` for zero nodes."""
+        CC = single_cell_complex(6)
+        assert len(CC.cells) == 1
+        assert (1, 2, 3, 4, 5) in CC.cells       
+
+class TestPyrmaidComplex:
+    """Test the `pyrmaid_complex` function."""
+    def test_pyrmaid_complex(self):
+        """Test `pyrmaid_complex` for zero nodes."""
+        CC = pyrmaid_complex(6)
+        assert len(CC.cells) == 6
+        assert (1, 2, 3, 4, 5) in CC.cells       
